065168391b28672669e7354238e1b2182c72269a
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-04-09 13:43:11 +0200

    rutas profile and groups, join edit create delete

--- models/group.js
+++ models/group.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
-    creator:[{type: Schema.Types.ObjectID, ref: 'User'}],
+    creator:{type: Schema.Types.ObjectID, ref: 'User'},
     name: {type: String, required: true},
     users: [{type: Schema.Types.ObjectID, ref: 'User'}],
     pending: [{type: Schema.Types.ObjectID, ref: 'User'}],
