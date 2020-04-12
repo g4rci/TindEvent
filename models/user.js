89385d96f83803230992e0a921f6cdc109b0be3f
Paola <paolavee@gmail.com>
2020-04-12 20:10:43 +0200

    added signup fields

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   username: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  date: {type: Date},
+  birthDate: {type: Date},
   location: {type: String},
   bio: {type: String},
   picture: { type: String, default: "https://banner2.cleanpng.com/20180404/djw/kisspng-computer-icons-users-group-internet-forum-user-avatar-5ac45a991206f5.9866985115228176890738.jpg"},
