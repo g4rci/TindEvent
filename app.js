8540af7507bdd18c7fa5e51ca35b745db84c6f18
M.A.Garcia <m.angel.gf04.com>
2020-04-16 10:20:45 +0200

    heroku

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://tind-event.firebaseapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
