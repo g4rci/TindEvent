ad986a4dc8537218f92b5f01f4fb2249f95499da
M.A.Garcia <m.angel.gf04.com>
2020-04-22 18:26:58 +0200

    ok

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000", "https://tindevent-mpm.web.app");
+  res.setHeader('Access-Control-Allow-Origin', "https://tindevent-mpm.web.app");
+  res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
