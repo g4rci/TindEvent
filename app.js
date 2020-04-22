58cf3e7001e9b810b31ba7d4f58a7904a044b80a
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-04-22 17:52:15 +0200

    New changes

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.PUBLIC_DOMAIN);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 // SESSION MIDDLEWARE
 app.use(
