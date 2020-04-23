d52bb0d8a318a0f2b1d652f82ae84887d38c8dd3
M.A.Garcia <m.angel.gf04.com>
2020-04-23 19:53:37 +0200

    file upload done

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:4000/upload", `${process.env.REACT_APP_API_URI}`);
+  res.setHeader('Access-Control-Allow-Origin', ("http://localhost:4000/", process.env.REACT_APP_API_URI));
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
